5b81d0113608b9bfb4690a4a7ace5e3139b66d83
Courel Charline <charline.courel@viacesi.fr>
2020-05-17 18:35:28 +0200

    ajout de getUsers

---
+++ Users/getUsers.php
@@ -0,0 +1,15 @@
+<?php
+    session_start(); // Vérifier si utile et si oui a quoi ça sert
+    require_once '../DbConnexion.php';
+
+    $query = $pdo->query("SELECT * FROM user");
+    $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    if(empty($result)){
+        echo 'Bad Request';
+    }
+    else{
+        echo json_encode($result,JSON_PRETTY_PRINT);
+    }
+     
+?>
